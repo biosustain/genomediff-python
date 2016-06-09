d79b88da58f3f86d49451113e64ccdb74e95182a
Lars Schöning <lars@lyschoening.de>
2016-06-09 14:45:30 +0200

    Release version 0.3.0 with fixes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='genomediff',
-    version='0.2.2',
+    version='0.3.0',
     packages=['genomediff'],
     url='https://github.com/biosustain/genomediff-python.git',
     license='MIT',
